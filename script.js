24ff7158d43ff0dba68791e53168ec8848e6372c
malekk2006 <malekalastal813@gmail.com>
2025-10-15 16:52:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
-
-  form.addEventListener("submit", function (e) {
-    // عرض رسالة تأكيد بعد الإرسال
-    alert("✅ تم إرسال التسجيل بنجاح! سيتم التواصل معك قريبًا.");
-  });
+  if (form) {
+    form.addEventListener("submit", function () {
+      alert("✅ تم إرسال التسجيل بنجاح! سيتم التواصل معك قريبًا.");
+    });
+  }
 });
